ed1b8155607172f52d60a24bef5d1fa68419df55
ChihYuChiang <ckccyu@gmail.com>
2018-08-19 17:06:23 +0800

    Update: marginal effect for research 3

--- bin/marginal_effect.R
+++ bin/marginal_effect.R
@@ -57,9 +57,10 @@ Model
 "
 lm_1 <- lm(Y1 ~ . + (x13 + x16 + x19) * x10, data=df1)
 lm_2 <- lm(y47 ~ . + (x3 + x12) * x17, data=df2)
-lm_equity <- lm(M11 ~ X11 * M22, data=dfEquity)
+lm_equity_1 <- lm(M11 ~ X11 * M22, data=dfEquity)
+lm_equity_2 <- lm(M12 ~ X11 * M22, data=dfEquity)
 
-summary(lm_equity)
+summary(lm_equity_2)
 
 
 
@@ -82,17 +83,18 @@ instant_effect(lm_2, target_var="x12", on_var="x17") %>%
 
 
 #--Line plot
-instant_effect(lm_equity, target_var="M22", on_var="X11") %>%
+instant_effect(lm_equity_2, target_var="M22", on_var="X11") %>%
   ggplot(aes(on_values, effect)) +
   geom_line() +
   geom_line(aes(y=effect - 1.96 * se), linetype=2) +
   geom_line(aes(y=effect + 1.96 * se), linetype=2) +
   geom_hline(yintercept=0) +
+  ylim(-0.125, 2.5) +
   labs(title="Marginal effect of equity investment",
-       subtitle="By ambidexterity imbalance",
-       x="Ambidexterity imbalance",
+       subtitle="By ambidexterity imbalance (citation view)",
+       x="Ambidexterity imbalance (citation view)",
        y="Estimated marginal effect") +
-  ggsave("../img/m22x11-2.png")
+  ggsave("../img/m22x11-2-citation.png")
 
 
 
